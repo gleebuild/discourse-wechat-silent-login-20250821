2a40664695d67f91ff4b7f9a2fea7be727679739
gleebuild <gleebuild@gmail.com>
2025-08-21 04:12:22 +0000

    Delete assets directory

--- assets/javascripts/discourse/routes/admin-plugins-wechat-logs.js
+++
@@ -1,8 +0,0 @@
-import Route from "@ember/routing/route";
-import { ajax } from "discourse/lib/ajax";
-
-export default class AdminPluginsWeChatLogsRoute extends Route {
-  model() {
-    return ajax("/wechat/admin/logs.json?limit=200");
-  }
-}
