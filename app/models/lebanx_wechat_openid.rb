72937f9b778d25b744e39e37d640d09e94a392e9
gleebuild <gleebuild@gmail.com>
2025-08-21 11:05:36 +0000

    Delete app directory

--- app/models/lebanx_wechat_openid.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-class LebanxWechatOpenid < ActiveRecord::Base
-  self.table_name = "lebanx_wechat_openid"
-
-  belongs_to :user
-
-  validates :openid, presence: true, uniqueness: true
-end
